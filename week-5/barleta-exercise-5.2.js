44e301b39653d0e5b9bfd758d47f8f2b569437a7
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-28 19:09:50 -0500

    commit

---
+++ week-5/barleta-exercise-5.2.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  barleta-exercise-5.2.js
+; Author: Professor Krasso
+; Date:   28 June 2020
+; Modified By: Marie Nicole Barleta
+; Description: Find errors advanced arrays
+;===========================================
+*/
+
+// import statement
+var header = require('../barleta-header.js');
+console.log(header.display("Marie", "Barleta", "Exercise 5.2"));
+console.log("\n")
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 5.2
+  Today's Date
+  Oysters
+  Shrimp
+  Steak
+  Tacos
+  Sushi
+*/
+
+//start program
+var foods = ['Oysters', 'Shrimp', 'Steak', 'Tacos', 'Sushi'];
+foods.forEach(function (food){
+console.log(food);
+})
+
+
+//end program
