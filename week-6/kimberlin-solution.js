aff233582fe1c93c01b41d688b126391b1433ceb
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-07-05 20:19:22 -0500

    commit

---
+++ week-6/kimberlin-solution.js
@@ -0,0 +1,38 @@
+/*
+ 	============================================
+ 	; Author: Rhiannon Kimberlin
+ 	; Date: 5 July 2020
+ 	; Modified By: Marie Nicole Barleta
+ 	; Description: Find errors OBJECTS & BUILT IN OBJECTS
+ 	;===========================================
+
+
+  /*
+  My solution:
+
+  1. Added parentheses to declare team message
+  2. Got rid of brackets on greatJob and needsImprovement.
+  2. Added colon to properly declare objects.
+  3. Got rid of parentheses on declared objects.
+
+  */
+
+
+	 //Expected Output
+	 /*Great job achieving benchmark results! That was possible through your commitment to excellence! Lets have another great month!
+
+	We didnt do as well as we were expecting. What input can you put in about why we did not achieve our goal so we can do better next month?*/
+
+  let teamMessage = ('Great job achieving benchmark results! That was possible through your commitment to excellence! Lets have another great month!');
+
+  let team = {
+    greatJob:
+      console.log(teamMessage),
+    needsImprovement:
+      console.log('We didnt do as well as we were expecting. What input can you put in about why we did not achieve our goal so we can do better next month?')
+
+  };
+
+  team.greatJob();
+  console.log("")
+  team.needsImprovement();
