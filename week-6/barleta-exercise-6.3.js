3d2a27612e2fc5bf6f72b35a97470f29a3844a76
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-07-05 19:47:51 -0500

    commit

---
+++ week-6/barleta-exercise-6.3.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  barleta-exercise-6.3.js
+; Author: Professor Krasso
+; Date:   5 July 2020
+; Modified By: Marie Nicole Barleta
+; Description: Exception handling
+; Demonstrates Object Literals
+;===========================================
+*/
+
+// import statement
+var header = require('../barleta-header.js');
+console.log(header.display("Marie", "Barleta", "Exercise 6.2"));
+console.log("\n")
+
+
+
+var request = {
+    id: 101,
+    name: "Help Desk Support",
+    requester: "Bob Jones"
+
+};
+
+
+console.log("id: " + request.id +", name: "+ request.name + ", requester: " + request.requester);
+
+
